1ba447fe684819e59ecb18199e2ab2a5dcd300b8
Oliver Kopp <kopp.dev@gmail.com>
2025-10-25 15:59:28 +0200

    Add library as heading

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 set -o pipefail
+
+echo "# $1" >> $GITHUB_STEP_SUMMARY
+echo "" >> $GITHUB_STEP_SUMMARY
+
+shift
+
 /jabref/jabkit/bin/jabkit "$@" 2>&1 | tee -a "$GITHUB_STEP_SUMMARY"
 exit ${PIPESTATUS[0]}
